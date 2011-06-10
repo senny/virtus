04c05c738798f385e1914d190a1e6b9e750f87fb
Piotr Solnica <piotr.solnica@gmail.com>
2011-06-10 12:05:10 +0200

    Add metrics

---
+++ metrics/flog.js
@@ -0,0 +1,12 @@
+              var g = new Bluff.Line('graph', "1000x600");
+      g.theme_37signals();
+      g.tooltips = true;
+      g.title_font_size = "24px"
+      g.legend_font_size = "12px"
+      g.marker_font_size = "10px"
+
+        g.title = 'Flog: code complexity';
+        g.data('average', [4.50808183291774]);
+        g.data('top 5% average', [17.1683600593384])
+        g.labels = {"0":"6/10"};
+        g.draw();
